d9a1a220a785eab3488963bfe60f7d06fd815ed2
Nillyr <ngy.cs@protonmail.com>
2021-08-01 13:25:06 +0200

    :sparkles: Adding automatic environment initialization on Windows

---
+++ setup.bat
@@ -0,0 +1,46 @@
+@echo off
+python --version >nul 2>&1
+if errorlevel 1 (
+  echo [x] Python is not installed >&2
+  exit /b
+)
+
+echo [+] Found Python
+
+python --version | findstr /R "[3]\.[3-9]" >nul 2>&1
+if errorlevel 1 (
+  echo [x] The minimum required version of python is python3.3 >&2
+  exit /b
+)
+
+python -m pip -V >nul
+if errorlevel 1 (
+  echo [x] pip not found >&2
+  exit /b
+)
+
+echo [+] Found pip
+echo [*] Running pip upgrade...
+python -m pip install --no-cache-dir --upgrade pip
+
+echo [*] Creating a virtual environment...
+rmdir "venv" /q /s >nul 2>&1
+python -m venv --clear .\venv
+if errorlevel 1 (
+  echo [x] Failed to create a virtual environment >&2
+  exit /b
+)
+
+set pythonvenv=.\venv\Scripts\python.exe
+%pythonvenv% -m pip install --upgrade pip
+
+echo [*] Installing the prerequisites...
+%pythonvenv% -m pip install --no-cache-dir -r requirements.txt
+
+if errorlevel 1 (
+  echo [x] Installation failed >&2
+  exit /b
+)
+
+echo [+] Installation complete!
+exit /b 0
