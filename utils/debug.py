abf87eafb716d63100249cc89de7e340cc7920b6
Nillyr <ngy.cs@protonmail.com>
2021-08-05 19:17:02 +0200

    :bug: Fix low impact bug

--- utils/debug.py
+++ utils/debug.py
@@ -1,21 +1,18 @@
 #!/usr/bin/env python
 
-from datetime import datetime
 from icecream import ic
 import sys
 
 class Debug():
   def __init__(self):
     self.debug = False
+    ic.disable()
 
   def set_debug(self, value: bool):
     self.debug = value
     if self.debug:
-      ic.configureOutput(prefix='{}:Debug:'.format(self.now()), includeContext=True)
+      ic.enable()
+      ic.configureOutput(prefix='Debug:', includeContext=True)
       return
-    ic.disable()
-
-  def now(self):
-    return f'[{datetime.now()}]'
 
 sys.modules[__name__] = Debug()
