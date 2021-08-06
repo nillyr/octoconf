cc9da9c76e102d940e0a6c66ad04627c5f3acf94
Nillyr <ngy.cs@protonmail.com>
2021-08-06 22:26:45 +0200

    :sparkles: Collection Script Generator for linux platform done

--- decorators/bash_decorator.py
+++ decorators/bash_decorator.py
@@ -6,10 +6,32 @@ class BashDecorator(Decorator):
   def decorator(func):
     def inner(*args, **kwargs):
       content = []
-      #TODO: add commands: create directory, get timestamp, check permissions
-      content.append("TODO: prolog")
+      prolog='''#!/bin/bash
+
+# Prolog
+echo \"[*] Permission check...\"
+if [[ $(/usr/bin/id -u) -ne 0 ]]; then
+  echo "This script must be run as root."
+  exit
+fi
+echo \"[+] OK!\"
+
+echo \"[*] Preparation...\"
+BASEDIR=$(/usr/bin/mktemp -d)
+echo `date` >> \"${BASEDIR}\"/timestamp.log
+exec 2>/dev/null
+
+# Configuration collection
+echo \"[*] Beginning of the collection...\"'''
+      content.append(prolog)
       content.extend(func(*args, **kwargs))
-      #TODO: add finish timestamp and zip
-      content.append("TODO: epilog")
+      epilog = '''
+# Epilog
+echo \"[*] Finishing...\"
+echo `date` >> \"${BASEDIR}\"/timestamp.log
+/usr/bin/tar zcf \"${BASEDIR##*/}\".tar.gz -C \"${BASEDIR}\" .
+/bin/rm -rf \"${BASEDIR}\"
+echo \"[+] Done!\"'''
+      content.append(epilog)
       return content
     return inner
