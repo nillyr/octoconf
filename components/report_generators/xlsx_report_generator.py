fecf06b294dd96c01684426572abda76d65ffefa
Nillyr <ngy.cs@protonmail.com>
2021-10-06 19:43:13 +0200

    :sparkles: Excel reporting is now available

--- components/report_generators/xlsx_report_generator.py
+++ components/report_generators/xlsx_report_generator.py
@@ -16,6 +16,9 @@ const.COLORS = {
 
 
 class XlsxGenerator(IReportGenerator):
+    def __init__(self):
+        self._synthesis = dict()
+
     def _get_category_format(self, workbook: xlsxwriter.workbook.Workbook):
         return workbook.add_format(
             {
@@ -81,7 +84,7 @@ class XlsxGenerator(IReportGenerator):
             }
         )
 
-    def _write_results(self, workbook: xlsxwriter.workbook.Workbook, data):
+    def _write_results(self, workbook: xlsxwriter.workbook.Workbook, data: list):
         for item in data:
             for category in item["categories"]:
                 worksheet = workbook.add_worksheet(name=category["name"])
@@ -91,6 +94,7 @@ class XlsxGenerator(IReportGenerator):
                     "A1:D1", category["name"], self._get_category_format(workbook)
                 )
                 checkpoint_row = 2
+                nb_passed, nb_failed, nb_na = 0, 0, 0
                 for checkpoint in category["checkpoints"]:
                     worksheet.merge_range(
                         f"A{checkpoint_row}:C{checkpoint_row}",
@@ -116,18 +120,21 @@ class XlsxGenerator(IReportGenerator):
                                     "PASSED",
                                     self._get_passed_result_format(workbook),
                                 )
+                                nb_passed += 1
                             elif check["result"] == False:
                                 worksheet.write(
                                     f"D{check_row}",
-                                    "PASSED",
+                                    "FAILED",
                                     self._get_failed_result_format(workbook),
                                 )
+                                nb_failed += 1
                             else:
                                 worksheet.write(
                                     f"D{check_row}",
                                     "N/A",
                                     self._get_uncertain_result_format(workbook),
                                 )
+                                nb_na += 1
                             check_row += 1
                             checkpoint_row = check_row
                         else:
@@ -136,18 +143,123 @@ class XlsxGenerator(IReportGenerator):
                                 "N/A",
                                 self._get_uncertain_result_format(workbook),
                             )
+                            nb_na += 1
                             check_row += 1
                             checkpoint_row = check_row
 
-    def _generate_charts(self, workbook: xlsxwriter.workbook.Workbook):
-        # TODO: (https://xlsxwriter.readthedocs.io/working_with_charts.html)
-        pass
+                self._synthesis[category["name"]] = {
+                    "PASSED": nb_passed,
+                    "FAILED": nb_failed,
+                    "N/A": nb_na,
+                }
 
-    def generate_report(self, data):
+    def _generate_synthesis(
+        self,
+        workbook: xlsxwriter.workbook.Workbook,
+        worksheet: xlsxwriter.worksheet.Worksheet,
+    ) -> int:
+        worksheet.set_column("A:G", 15)
+        worksheet.set_row(0, 25)
+        worksheet.merge_range("A1:G1", "Synthesis", self._get_category_format(workbook))
+        worksheet.merge_range(
+            "A2:C2", "Categories", self._get_checkpoint_format(workbook)
+        )
+        worksheet.write("D2", "# PASSED", self._get_checkpoint_format(workbook))
+        worksheet.write("E2", "# FAILED", self._get_checkpoint_format(workbook))
+        worksheet.write("F2", "# N/A", self._get_checkpoint_format(workbook))
+        worksheet.write("G2", "% Percentage", self._get_checkpoint_format(workbook))
+
+        row = 2
+        for key, value in self._synthesis.items():
+            row += 1
+            ic(key, value["PASSED"], value["FAILED"], value["N/A"])
+            worksheet.merge_range(
+                f"A{row}:C{row}",
+                key,
+                self._get_check_format(workbook),
+            )
+            worksheet.write(
+                f"D{row}",
+                value["PASSED"],
+                self._get_check_format(workbook),
+            )
+            worksheet.write(
+                f"E{row}",
+                value["FAILED"],
+                self._get_check_format(workbook),
+            )
+            worksheet.write(
+                f"F{row}",
+                value["N/A"],
+                self._get_check_format(workbook),
+            )
+            worksheet.write(
+                f"G{row}",
+                (value["PASSED"] * 100)
+                / (value["PASSED"] + value["FAILED"] + value["N/A"]),
+                self._get_check_format(workbook),
+            )
+        return row
+
+    def _generate_charts(
+        self,
+        workbook: xlsxwriter.workbook.Workbook,
+        worksheet: xlsxwriter.worksheet.Worksheet,
+        last_row: int,
+    ):
+        # Radar
+        radar_chart = workbook.add_chart({"type": "radar", "subtype": "filled"})
+        radar_chart.set_title({"name": "Configuration coverage summary"})
+        radar_chart.add_series(
+            {
+                "name": "=Synthesis!$G$2",
+                "categories": f"=Synthesis!$A$3:$A${last_row + 1}",
+                "values": f"=Synthesis!$G$3:$G${last_row + 1}",
+                "fill": {"color": "green"},
+            }
+        )
+        worksheet.insert_chart("J1", radar_chart)
+
+        # Column
+        column_chart = workbook.add_chart({"type": "column"})
+        column_chart.set_title({"name": "Configuration coverage summary"})
+        column_chart.set_x_axis({"name": "Categories"})
+        column_chart.set_y_axis({"name": "Number of checks"})
+        column_chart.add_series(
+            {
+                "name": "=Synthesis!$D$2",
+                "categories": f"=Synthesis!$A$3:$A${last_row}",
+                "values": f"=Synthesis!$D$3:$D${last_row}",
+                "fill": {"color": "green"},
+            }
+        )
+        column_chart.add_series(
+            {
+                "name": "=Synthesis!$E$2",
+                "categories": f"=Synthesis!$A$3:$A${last_row}",
+                "values": f"=Synthesis!$E$3:$E${last_row}",
+                "fill": {"color": "red"},
+            }
+        )
+        column_chart.add_series(
+            {
+                "name": "=Synthesis!$F$2",
+                "categories": f"=Synthesis!$A$3:$A${last_row}",
+                "values": f"=Synthesis!$F$3:$F${last_row}",
+                "fill": {"color": "blue"},
+            }
+        )
+        column_chart.set_table({"show_keys": True})
+        column_chart.set_legend({"position": "none"})
+        worksheet.insert_chart(f"A{last_row + 5}", column_chart)
+
+    def generate_report(self, data: list):
         filename = timestamp() + "_results"
         workbook = xlsxwriter.Workbook(filename + ".xlsx")
+        worksheet = workbook.add_worksheet(name="Synthesis")
         self._write_results(workbook, data)
-        self._generate_charts(workbook)
+        last_row = self._generate_synthesis(workbook, worksheet)
+        self._generate_charts(workbook, worksheet, last_row)
         workbook.close()
 
         filename += ".json"
