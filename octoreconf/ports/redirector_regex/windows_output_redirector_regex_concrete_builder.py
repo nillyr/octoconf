fb315505f6a8712b4434daa835187f905003ab29
Nillyr <ngy.cs@protonmail.com>
2021-11-22 18:25:58 +0100

    :sparkles: Added Export-CSV cmdlet regex

--- octoreconf/ports/redirector_regex/windows_output_redirector_regex_concrete_builder.py
+++ octoreconf/ports/redirector_regex/windows_output_redirector_regex_concrete_builder.py
@@ -36,6 +36,7 @@ class WindowsOutputRedirectorRegexCB(IOutputRedirectorRegexBuilder):
         BASICS = "\s*(([1-6]>\&[1-6]\s*)*|[1-6]|[\*])>+\s*"
         CMD_GPRESULT = "\s*/H\s*"
         CMD_SECEDIT = "\s*/cfg\s*"
+        CMDLET_EXPORT_CSV = "\s*\|\s*Export-CSV\s+(-[a-zA-Z]*\s*)*"
         CMDLET_OUT_FILE = "\s*\|\s*Out-File\s+(-[a-zA-Z]*\s*(?:utf8\s*)*)*"
         CMDLET_TEE_OBJECT = "\s*\|\s*Tee-Object\s+(-((InputObject\s+[a-zA-Z0-9_-]*\s+)|([a-zA-Z]*\s*(?:utf8\s*)*)))*"
 
