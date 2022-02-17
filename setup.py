456a6b550fea6d9ddd042837cd8c7c06d4a3d0e2
Nicolas GRELLETY <ngy.cs@protonmail.com>
2022-02-17 23:45:18 +0100

    :construction_worker: buid: add github action to run tests
    Signed-off-by: Nicolas GRELLETY <ngy.cs@protonmail.com>

--- setup.py
+++ setup.py
@@ -2,15 +2,23 @@ import os
 
 from setuptools import setup, find_packages
 
-from octoconf.__init__ import __version__
+# from octoconf.__init__ import __version__
+
 
 current_dir = os.path.abspath(os.path.dirname(__file__))
+
+# Quick and dirty fix because tox doesn't like from octoconf.[...]
+with open(os.path.join(current_dir, "octoconf/__init__.py"), "r") as f:
+    version = f.readlines()[0].replace("__version__ = ", "").replace('"', "")
+
+
 with open(os.path.join(current_dir, "requirements.txt"), "r") as f:
     requirements = f.readlines()
 
 setup(
     name="octoconf",
-    version=__version__,
+    # version=__version__,
+    version=version,
     description="Tool for semi-automatic verification of security configurations.",
     url="https://github.com/nillyr/octoconf",
     author="Nicolas GRELLETY",
