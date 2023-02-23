f46f12bdf35873b399e89b146fe26391e7c3b989
Nicolas GRELLETY <ngy.cs@protonmail.com>
2023-02-23 19:43:53 +0100

    :white_check_mark: add unit test for regex

---
+++ octoconf_tests/interfaces/test_unix_redirectors.py
@@ -0,0 +1,174 @@
+# @copyright Copyright (c) 2021-2023 Nicolas GRELLETY
+# @license https://opensource.org/licenses/GPL-3.0 GNU GPLv3
+# @link https://github.com/nillyr/octoconf
+# @since 1.0.0b
+
+import sys
+
+import pytest
+
+sys.path.append("../../octoconf/")
+from octoconf.interfaces.generate_script.unix_script import IUnixScript
+
+
+@pytest.fixture
+def unix_basics_redirectors():
+    return [
+        "id>id.txt",
+        "id> id.txt",
+        "id >id.txt",
+        "id > id.txt",
+        "id >| id.txt",
+        "id>>id.txt",
+        "id>> id.txt",
+        "id >>id.txt",
+        "id >> id.txt",
+        "id1>id.txt",
+        "id1> id.txt",
+        "id 1>id.txt",
+        "id 1> id.txt",
+        "id1>>id.txt",
+        "id1>> id.txt",
+        "id 1>>id.txt",
+        "id 1>> id.txt",
+        "id&>id.txt",
+        "id&> id.txt",
+        "id &>id.txt",
+        "id &> id.txt",
+        "id 2>&1 > id.txt",
+        "id 2>log_stderr.txt >|id.txt",
+        "cat << EOF>here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF> here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF >here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF > here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF>>here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF>> here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF >>here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat << EOF >> here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat <<- EOF>here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat <<- EOF> here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat <<- EOF >here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        "cat <<- EOF > here_document.txt\ndummy line 1\ndummy line 2\nEOF",
+        'if (( $a > $b )); then echo "true" > comparison_operators.txt; fi',
+        "(echo $(whoami |tee whoami.txt); id > id.txt; )",
+        'id > id.txt; if (( 42 > 24 )); then echo "true" > comp.txt; fi',
+    ]
+
+
+@pytest.fixture
+def unix_basics_redirectors_expected():
+    return [
+        'id>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id >"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id > "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id >| "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id>>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id>> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id >>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id >> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id1>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id1> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 1>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 1> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id1>>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id1>> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 1>>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 1>> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id&>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id&> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id &>"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id &> "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 2>&1 > "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id 2>"${BASEDIR}"/"${CATEGORY}"/log_stderr.txt >|"${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'cat << EOF>"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF> "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF >"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF > "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF>>"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF>> "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF >>"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat << EOF >> "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat <<- EOF>"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat <<- EOF> "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat <<- EOF >"${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'cat <<- EOF > "${BASEDIR}"/"${CATEGORY}"/here_document.txt\ndummy line 1\ndummy line 2\nEOF',
+        'if (( $a > $b )); then echo "true" > "${BASEDIR}"/"${CATEGORY}"/comparison_operators.txt; fi',
+        '(echo $(whoami |tee "${BASEDIR}"/"${CATEGORY}"/whoami.txt); id > "${BASEDIR}"/"${CATEGORY}"/id.txt; )',
+        'id > "${BASEDIR}"/"${CATEGORY}"/id.txt; if (( 42 > 24 )); then echo "true" > "${BASEDIR}"/"${CATEGORY}"/comp.txt; fi',
+    ]
+
+
+def test_unix_basics_redirectors(
+    unix_basics_redirectors, unix_basics_redirectors_expected
+):
+    output = []
+    for cmd in unix_basics_redirectors:
+        output.append(
+            IUnixScript.preprocess_collection_cmd('"${BASEDIR}"/"${CATEGORY}"/', cmd)
+        )
+    assert output == unix_basics_redirectors_expected
+
+
+@pytest.fixture
+def unix_fd_to_files_redirectors():
+    return [
+        "exec 3>fd_to_file.txt\nid >&3\nexec 3>&-",
+        "echo 1234567890 > file.txt\nexec 3<>file.txt\nread -rn 4 <&3\necho -n . >&3\nexec 3>&-",
+    ]
+
+
+@pytest.fixture
+def unix_fd_to_files_redirectors_expected():
+    return [
+        'exec 3>"${BASEDIR}"/"${CATEGORY}"/fd_to_file.txt\nid >&3\nexec 3>&-',
+        'echo 1234567890 > "${BASEDIR}"/"${CATEGORY}"/file.txt\nexec 3<>"${BASEDIR}"/"${CATEGORY}"/file.txt\nread -rn 4 <&3\necho -n . >&3\nexec 3>&-',
+    ]
+
+
+def test_unix_fd_to_files_redirectors(
+    unix_fd_to_files_redirectors, unix_fd_to_files_redirectors_expected
+):
+    output = []
+    for cmd in unix_fd_to_files_redirectors:
+        output.append(
+            IUnixScript.preprocess_collection_cmd('"${BASEDIR}"/"${CATEGORY}"/', cmd)
+        )
+    assert output == unix_fd_to_files_redirectors_expected
+
+
+@pytest.fixture
+def unix_tee_cmd_redirectors():
+    return [
+        "id | tee id.txt",
+        "id | tee -a id.txt",
+        "id | tee -i id.txt",
+        "id | tee -i -a id.txt",
+        "id | tee -p id.txt",
+        "id | tee -p -i id.txt",
+        "id | tee -p -i -a id.txt",
+    ]
+
+
+@pytest.fixture
+def unix_tee_cmd_redirectors_expected():
+    return [
+        'id | tee "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -a "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -i "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -i -a "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -p "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -p -i "${BASEDIR}"/"${CATEGORY}"/id.txt',
+        'id | tee -p -i -a "${BASEDIR}"/"${CATEGORY}"/id.txt',
+    ]
+
+
+def test_unix_tee_cmd_redirectors(
+    unix_tee_cmd_redirectors, unix_tee_cmd_redirectors_expected
+):
+    output = []
+    for cmd in unix_tee_cmd_redirectors:
+        output.append(
+            IUnixScript.preprocess_collection_cmd('"${BASEDIR}"/"${CATEGORY}"/', cmd)
+        )
+    assert output == unix_tee_cmd_redirectors_expected
---
+++ octoconf_tests/interfaces/test_windows_redirectors.py
@@ -0,0 +1,231 @@
+# @copyright Copyright (c) 2021-2023 Nicolas GRELLETY
+# @license https://opensource.org/licenses/GPL-3.0 GNU GPLv3
+# @link https://github.com/nillyr/octoconf
+# @since 1.0.0b
+
+import sys
+
+import pytest
+
+sys.path.append("../../octoconf/")
+from octoconf.interfaces.generate_script.windows_script import IWindowsScript
+
+
+@pytest.fixture
+def windows_basics_redirectors():
+    return [
+        "whoami > whoami.txt",
+        "whoami >> whoami.txt",
+        "whoami 1> whoami.txt",
+        "whoami 3> whoami.txt",
+        "whoami *> whoami.txt",
+        "whoami 4>&1 > whoami.txt",
+        "whoami 4>&1 >> whoami.txt",
+        "whoami *>&1 > whoami.txt",
+        "whoami *>&1 >> whoami.txt",
+        '&{\nWrite-Warning "hello"\nWrite-Error "hello"\nWrite-Output "hello"\n} 3>&1 2>&1 > redirection.txt',
+        'if ($a > $b) { "true" } elseif ( $c > $d ) { "true" } else { "false" } > comparison_operators.txt',
+        '$(try{ Get-Content a_file.txt *> a_another_file.txt; Write-Output "hi" }catch{$Error[0]}) | Out-File -FilePath output.txt',
+        '$(try{ Get-Content a_file.txt *> a_another_file.txt; if ($a > $b) { "true" } elseif ( $c > $d ) { "true" } else { "false" } } catch { $Error[0] }) | Out-File -FilePath output.txt',
+    ]
+
+
+@pytest.fixture
+def windows_basics_redirectors_expected():
+    return [
+        "whoami > $basedir\\$category\\whoami.txt",
+        "whoami >> $basedir\\$category\\whoami.txt",
+        "whoami 1> $basedir\\$category\\whoami.txt",
+        "whoami 3> $basedir\\$category\\whoami.txt",
+        "whoami *> $basedir\\$category\\whoami.txt",
+        "whoami 4>&1 > $basedir\\$category\\whoami.txt",
+        "whoami 4>&1 >> $basedir\\$category\\whoami.txt",
+        "whoami *>&1 > $basedir\\$category\\whoami.txt",
+        "whoami *>&1 >> $basedir\\$category\\whoami.txt",
+        '&{\nWrite-Warning "hello"\nWrite-Error "hello"\nWrite-Output "hello"\n} 3>&1 2>&1 > $basedir\\$category\\redirection.txt',
+        'if ($a > $b) { "true" } elseif ( $c > $d ) { "true" } else { "false" } > $basedir\\$category\\comparison_operators.txt',
+        '$(try{ Get-Content a_file.txt *> $basedir\\$category\\a_another_file.txt; Write-Output "hi" }catch{$Error[0]}) | Out-File -FilePath $basedir\\$category\\output.txt',
+        '$(try{ Get-Content a_file.txt *> $basedir\\$category\\a_another_file.txt; if ($a > $b) { "true" } elseif ( $c > $d ) { "true" } else { "false" } } catch { $Error[0] }) | Out-File -FilePath $basedir\\$category\\output.txt',
+    ]
+
+
+def test_windows_basics_redirectors(
+    windows_basics_redirectors, windows_basics_redirectors_expected
+):
+    output = []
+    for cmd in windows_basics_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_basics_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmd_gpresult_redirectors():
+    return [
+        "gpresult /H gpreport.html",
+        "gpresult /Scope computer /H gpreport.html",
+    ]
+
+
+@pytest.fixture
+def windows_cmd_gpresult_redirectors_expected():
+    return [
+        "gpresult /H $basedir\\$category\\gpreport.html",
+        "gpresult /Scope computer /H $basedir\\$category\\gpreport.html",
+    ]
+
+
+def test_windows_cmd_gpresult_redirectors(
+    windows_cmd_gpresult_redirectors, windows_cmd_gpresult_redirectors_expected
+):
+    output = []
+    for cmd in windows_cmd_gpresult_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmd_gpresult_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmd_secedit_redirectors():
+    return [
+        "secedit /export /cfg secedit.txt",
+    ]
+
+
+@pytest.fixture
+def windows_cmd_secedit_redirectors_expected():
+    return [
+        "secedit /export /cfg $basedir\\$category\\secedit.txt",
+    ]
+
+
+def test_windows_cmd_secedit_redirectors(
+    windows_cmd_secedit_redirectors, windows_cmd_secedit_redirectors_expected
+):
+    output = []
+    for cmd in windows_cmd_secedit_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmd_secedit_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmd_msinfo32_redirectors():
+    return [
+        "msinfo32.exe /report report.txt",
+    ]
+
+
+@pytest.fixture
+def windows_cmd_msinfo32_redirectors_expected():
+    return [
+        "msinfo32.exe /report $basedir\\$category\\report.txt",
+    ]
+
+
+def test_windows_cmd_msinfo32_redirectors(
+    windows_cmd_msinfo32_redirectors, windows_cmd_msinfo32_redirectors_expected
+):
+    output = []
+    for cmd in windows_cmd_msinfo32_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmd_msinfo32_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmdlet_export_csv_redirectors():
+    return [
+        "Get-Process -Name WmiPrvSE | Export-CSV -Path WmiData.csv -NoTypeInformation",
+        "Get-Process -Name WmiPrvSE | Export-CSV -Encoding utf8 -Path WmiData.csv -NoTypeInformation",
+    ]
+
+
+@pytest.fixture
+def windows_cmdlet_export_csv_redirectors_expected():
+    return [
+        "Get-Process -Name WmiPrvSE | Export-CSV -Path $basedir\\$category\\WmiData.csv -NoTypeInformation",
+        "Get-Process -Name WmiPrvSE | Export-CSV -Encoding utf8 -Path $basedir\\$category\\WmiData.csv -NoTypeInformation",
+    ]
+
+
+def test_windows_cmdlet_export_csv_redirectors(
+    windows_cmdlet_export_csv_redirectors,
+    windows_cmdlet_export_csv_redirectors_expected,
+):
+    output = []
+    for cmd in windows_cmdlet_export_csv_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmdlet_export_csv_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmdlet_out_file_redirectors():
+    return [
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -FilePath WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -FilePath WmiPrvSE.txt -NoClobber",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -Append -FilePath WmiPrvSE.txt",
+    ]
+
+
+@pytest.fixture
+def windows_cmdlet_out_file_redirectors_expected():
+    return [
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -FilePath $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -FilePath $basedir\\$category\\WmiPrvSE.txt -NoClobber",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Out-File -Encoding utf8 -Append -FilePath $basedir\\$category\\WmiPrvSE.txt",
+    ]
+
+
+def test_windows_cmdlet_out_file_redirectors(
+    windows_cmdlet_out_file_redirectors,
+    windows_cmdlet_out_file_redirectors_expected,
+):
+    output = []
+    for cmd in windows_cmdlet_out_file_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmdlet_out_file_redirectors_expected
+
+
+@pytest.fixture
+def windows_cmdlet_tee_object_redirectors():
+    return [
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 -FilePath WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 -FilePath WmiPrvSE.txt -Append",
+    ]
+
+
+@pytest.fixture
+def windows_cmdlet_tee_object_redirectors_expected():
+    return [
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 -FilePath $basedir\\$category\\WmiPrvSE.txt",
+        "Get-Process -Name WmiPrvSE | ft -autosize -wrap | Tee-Object -Encoding utf8 -FilePath $basedir\\$category\\WmiPrvSE.txt -Append",
+    ]
+
+
+def test_windows_cmdlet_tee_object_redirectors(
+    windows_cmdlet_tee_object_redirectors,
+    windows_cmdlet_tee_object_redirectors_expected,
+):
+    output = []
+    for cmd in windows_cmdlet_tee_object_redirectors:
+        output.append(
+            IWindowsScript.preprocess_collection_cmd("$basedir\\$category\\", cmd)
+        )
+    assert output == windows_cmdlet_tee_object_redirectors_expected
